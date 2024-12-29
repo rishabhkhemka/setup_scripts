a11508ba4bc9a52a8f69f1a47ae59a50e842544a
rishabh khemka <70965669+rishabhkhemka@users.noreply.github.com>
2024-12-29 15:56:30 +0530

    fixed zsh script to use theme from the right path

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -5,7 +5,6 @@ handle_error() {
     exit 1
 }
 
-
 install_zsh() {
     echo "Installing Zsh and Powerline fonts..."
     sudo apt install -y zsh fonts-powerline || handle_error "install_zsh_dependencies"
@@ -15,28 +14,37 @@ install_zsh() {
 
 install_theme() {
     echo "Installing Oh My Zsh and Powerlevel10k..."
-    sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || handle_error "install_oh_my_zsh"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k || handle_error "install_powerlevel10k"
+    sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended \
+        || handle_error "install_oh_my_zsh"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k \
+        || handle_error "install_powerlevel10k"
 }
 
 configure_powerlevel10k() {
     echo "Configuring Powerlevel10k..."
-    if [ -f ~/.p10k.zsh ]; then
-        echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc || handle_error "configure_powerlevel10k_append"
+    if [ -f ~/setup_scripts/.p10k.zsh ]; then
+        cp ~/setup_scripts/.p10k.zsh ~/ || handle_error "copy_p10k_zsh"
+        echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc \
+            || handle_error "configure_powerlevel10k_append"
     else
-        echo ".p10k.zsh file not found. Skipping Powerlevel10k configuration."
+        echo "~/setup_scripts/.p10k.zsh file not found. Skipping Powerlevel10k configuration."
     fi
 
     # Set the theme in .zshrc
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc || handle_error "configure_powerlevel10k_theme"
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc \
+        || handle_error "configure_powerlevel10k_theme"
 }
 
 install_plugins() {
     echo "Installing Zsh plugins..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions || handle_error "install_autosuggestions"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || handle_error "install_syntax_highlighting"
-    ~/.oh-my-zsh/custom/themes/powerlevel10k/gitstatus/install || handle_error "install gitstatusd"
-    sed -i '/^plugins=(/ s/plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting /' ~/.zshrc || handle_error "update_zshrc_plugins"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
+        || handle_error "install_autosuggestions"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting \
+        || handle_error "install_syntax_highlighting"
+    ~/.oh-my-zsh/custom/themes/powerlevel10k/gitstatus/install \
+        || handle_error "install gitstatusd"
+    sed -i '/^plugins=(/ s/plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting /' ~/.zshrc \
+        || handle_error "update_zshrc_plugins"
 }
 
 main() {
